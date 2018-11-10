a6c5cd4adb04bfb3e24e5d45e228efb409fe5977
Tuan Nguyen <tuan.nguyen@isentia.com>
2018-11-10 11:20:40 +0700

    5. Express Router

--- src/index.js
+++ src/index.js
@@ -2,10 +2,15 @@
 // src/index.js
 
 const express = require("express");
+
+const usersRouter = require("./routes");
+
 const app = express();
 const port = 3000;
 
+// Thay thế cho app.use("/", (req, res) => res.send("Hello World!"))
 app.get("/", (req, res) => res.send("Hello World!"));
+app.use("/users/", usersRouter);
 
 app.listen(port, () =>
   console.log(`Server running at http://127.0.0.1:${port}/`)
---
+++ src/routes.js
@@ -0,0 +1,24 @@
+// Nodeplusplus.com
+// src/routes.js
+
+const express = require("express");
+
+const router = express.Router();
+router.post("/local/register", (req, res) => res.send("GET /local/register"));
+router.put("/local/update", (req, res) => res.send("PUT /local/update"));
+router.post("/local/reset-password", (req, res) =>
+  res.send("POST /local/reset-password")
+);
+router.get("/local/:token", (req, res) => res.send("GET /local/:token TEST"));
+router.post("/local/login", (req, res) => res.send("POST /local/login"));
+router.post("/local/password-recovery", (req, res) =>
+  res.send("POST /local/password-recovery")
+);
+router.get("GET /local/password-recovery/:token", (req, res) =>
+  res.send("/local/password-recovery/:token")
+);
+router.put("PUT /local/password-recovery/:token", (req, res) =>
+  res.send("/local/password-recovery/:token")
+);
+
+module.exports = router;
