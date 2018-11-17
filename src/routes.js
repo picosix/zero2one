23d9ed4c399025cf912384909ac7bfe54dd1c6e2
Tuan Nguyen <tuan.nguyen@isentia.com>
2018-11-17 09:24:47 +0700

    Remove test code

--- src/routes.js
+++ src/routes.js
@@ -9,7 +9,7 @@ router.put("/local/update", (req, res) => res.send("PUT /local/update"));
 router.post("/local/reset-password", (req, res) =>
   res.send("POST /local/reset-password")
 );
-router.get("/local/:token", (req, res) => res.send("GET /local/:token TEST"));
+router.get("/local/:token", (req, res) => res.send("GET /local/:token"));
 router.post("/local/login", (req, res) => res.send("POST /local/login"));
 router.post("/local/password-recovery", (req, res) =>
   res.send("POST /local/password-recovery")
