a3a165b1d4882ada20181ad9733293960f3bb69b
Tuan Nguyen <tuan.nguyen@isentia.com>
2018-11-17 10:04:08 +0700

    6. Config Mongoose

--- src/index.js
+++ src/index.js
@@ -2,16 +2,25 @@
 // src/index.js
 
 const express = require("express");
+const mongoose = require("mongoose");
 
 const usersRouter = require("./routes");
 
 const app = express();
-const port = 3000;
+app.set("port", 3000); // <- Cấu hình
+
+const mongoURL = "mongodb://localhost:27017/zero2one";
+mongoose.connect(
+  mongoURL,
+  { useNewUrlParser: true }
+);
+app.set("mongoose", mongoose);
 
 // Thay thế cho app.use("/", (req, res) => res.send("Hello World!"))
 app.get("/", (req, res) => res.send("Hello World!"));
 app.use("/users/", usersRouter);
 
-app.listen(port, () =>
-  console.log(`Server running at http://127.0.0.1:${port}/`)
+app.listen(app.get("port"), () =>
+  // <- Gọi lại biến đã cấu hình
+  console.log(`Server running at http://127.0.0.1:${app.get("port")}/`)
 );
